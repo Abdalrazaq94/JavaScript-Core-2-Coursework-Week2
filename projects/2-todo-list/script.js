c8be2a0a662e93376f01b43a3e0f60b010546edf
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-01-27 19:49:33 +0000

    modidied

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -2,7 +2,7 @@
 function populateTodoList(todos) {
   
   
-    todos.forEach(addToDoo)
+   todos.forEach(addToDoo);
 
 }
 
@@ -14,17 +14,21 @@ function addToDoo (add) {
   let list = document.querySelector('#todo-list');
   let li = document.createElement('li');
   li.innerHTML = add.task
+  console.log(add.task);
   li.className = 'list-group-item d-flex justify-content-between align-items-center'
   list.appendChild(li)
   let span = document.createElement('span')
   span.classList =  'badge bg-primary rounded-pill'
   li.appendChild(span);
-  let a = document.createElement('i')
-  a.className = 'fa fa-check'
-  span.appendChild(a)
-  // let iT = document.createElement('i')
-  // a.className = 'fa fa-trash'
-  // span.appendChild(iT)
+  const completedButton = document.createElement("button");
+  completedButton.classList = 'fa fa-check'
+  span.appendChild(completedButton);
+  //Create trash button
+  const trashButton = document.createElement("button");
+ 
+  trashButton.classList = 'fa fa-trash'
+  span.appendChild(trashButton);
+  
 }
 
 
